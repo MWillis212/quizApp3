597972c71afda0b549e2887cd8a35ee5680ddc14
Miles <willismiles96@gmail.com>
2021-05-14 18:22:03 -0400

    v3

--- Quizapp3.java
+++ Quizapp3.java
@@ -1,35 +1,50 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class Quizapp3 {
- public static void main(String[] args) {
-   
-	  boolean y = true, n = false;  
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Enter a grade  :");
-    int grade1 = scan.nextInt();
-     System.out.println(grade1);
-   
-     Scanner scan1 = new Scanner(System.in);
-     System.out.println("Would you like to enter more grades : [y/n]");
-    
-    String response = scan1.nextLine();
-    
-     if( response.charAt(0) == 'y') {  System.out.println("Enter a grade  :");
-    int grade2 = scan.nextInt();
-     System.out.println(grade2);
-     System.out.println("Would you like to enter more grades : [y/n]");
-     
-     } 
-    	
-   
-    
-    	
-  }
- }
-       
-// If no, system would print x students grades>= 90
-// then, system println  X students grades =<90 while >=80
-// then , system println X Students grades >=70 while 79
-// then,  system println xstudents  <=69
-   
+
+	public static void main(String[] args) {
+
+		boolean y = true, n = false;
+
+		Scanner scan1 = new Scanner(System.in);
+		// array list
+		ArrayList<Integer> gradelist = new ArrayList<>();
+
+		while (true) {
+		
+			System.out.println("Enter a grade  :");
+
+			int grade = scan1.nextInt();
+
+			gradelist.add(grade);
+			
+			System.out.println("Would you like to enter more grades : [y/n]");
+			scan1.nextLine(); 
+			String response = scan1.nextLine();
+
+			if (response.startsWith("y")) continue;
+				
+
+			if (response.startsWith("n"))	break;
+			
+			
+		}
+			int highestgrade = Collections.max(gradelist);
+			int lowestgrade = Collections.min(gradelist);
+			double averagegrade = gradelist.stream().mapToInt(s->s).average().orElse(0.0);
+			long numA = gradelist.stream().count();
+					 
+			
+		// What was the highest score?
+		// What was the lowest score?
+		// What was the average score for the class?
+		// How many students got an A (90+)?
+		// How many students got a B (80 - 89)?
+		// How many students got a C (70 - 79)?
+		// How many students failed (69 or lower)?
+
+	}
+}
